d5345e3244da200d9b27a9a675298491026eb4fd
Anukool Purohit <anukoolpurohit@gmail.com>
2021-09-15 16:10:31 +0530

    feat: Add run_experiment.py

--- handwritingBCI/models/simple_rnn.py
+++ handwritingBCI/models/simple_rnn.py
@@ -3,13 +3,21 @@ import torch.nn.functional as F
 
 
 class SimpleRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int,
+                 num_layers: int = 1, bidirectional: bool = False):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
+        self.num_layers = num_layers
+        self.bidirectional = bidirectional
 
-        self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.num_directions = int(self.bidirectional) + 1
+
+        self.rnn = nn.LSTM(input_size, hidden_size,
+                           num_layers=self.num_layers,
+                           bidirectional=self.bidirectional,
+                           batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
         return
 
---
+++ training/run_experiment.py
@@ -0,0 +1,26 @@
+from torch import nn
+from handwritingBCI.data.datasets import NeuroDataset
+from handwritingBCI.data.utils.transforms import get_simple_transforms
+from handwritingBCI.data.databunch import Databunch
+from handwritingBCI.pathlib_extension import Path
+from handwritingBCI.training.trainer import Trainer
+from handwritingBCI.training.metrics import Loss, Accuracy
+from handwritingBCI.models.simple_rnn import SimpleRNN
+
+
+PATH = Path("/home/anukoolpurohit/Documents/AnukoolPurohit/Datasets/HandwritingBCI/handwriting-bci/handwritingBCIData")
+
+neuro_dataset = NeuroDataset.from_path(PATH, get_transforms=get_simple_transforms)
+
+batch_size = 32
+test_size = 0.1
+
+data = Databunch.from_neuro_dataset(neuro_dataset, test_size=test_size, batch_size=batch_size)
+model = SimpleRNN(192, 64, 31)
+
+epochs = 100
+lr = 1e-3
+loss_func = nn.CrossEntropyLoss()
+
+trainer = Trainer(data, model, loss_func=loss_func, lr=lr, metrics=[Loss, Accuracy])
+trainer.tune(epochs=epochs)
